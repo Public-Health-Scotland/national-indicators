94c8ef3345cedf5c00dc4f21d7616194471d8a9f
shintoLamp <bateman.mcbride@phs.scot>
2022-11-09 07:44:30 +0000

    Tests for date functions and partnership names

---
+++ tests/testthat/test-standardise_partnership_names.R
@@ -0,0 +1,19 @@
+test_that("Partnership names are modified correctly", {
+
+  dummy_data <- tibble::tribble(
+    ~partnership,
+    "Aberdeen City",
+    "Perth & Kinross",
+    "Orkney",
+    "Shetland",
+    "City of Edinburgh",
+    "Na h-Eileanan Siar"
+  )
+
+  expect_equal(
+   standardise_partnership_names(dummy_data$partnership),
+    c("Aberdeen City", "Perth and Kinross", "Orkney Islands", "Shetland Islands",
+      "Edinburgh", "Western Isles")
+  )
+
+})
