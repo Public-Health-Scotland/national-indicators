c145d63098c94260178412860ddebcd3f05724f6
shintoLamp <bateman.mcbride@phs.scot>
2022-11-09 07:42:43 +0000

    General function for partnership names

---
+++ R/standardise_partnership_names.R
@@ -0,0 +1,19 @@
+#' Standardise partnership names for NIs
+#'
+#' @param partnership_name A vector of existing poartnership names
+#'
+#' @return A vector of partnership names in the NI expected format
+#' @export
+standardise_partnership_names <- function(partnership_name) {
+  fixed_partnership <-
+    dplyr::case_when(
+      stringr::str_detect(partnership_name, "^City of Edinburgh$") ~ "Edinburgh",
+      partnership_name == "Edinburgh, City of" ~ "Edinburgh",
+      partnership_name == "Na h-Eileanan Siar" ~ "Western Isles",
+      stringr::str_detect(partnership_name, "&") ~ stringr::str_replace(partnership_name, "&", "and"),
+      stringr::str_detect(partnership_name, "(^Orkney$|^Shetland$)") ~
+        stringr::str_replace(partnership_name, "(^Orkney$|^Shetland$)", "\\1 Islands"),
+      TRUE ~ partnership_name
+    )
+  return(fixed_partnership)
+}
