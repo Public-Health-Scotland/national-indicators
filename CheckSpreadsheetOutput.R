b5d7d233af1dabc0f5bea60a877afdfc56e5ad94
Bateman McBride <105846071+shintoLamp@users.noreply.github.com>
2022-11-10 10:28:59 +0000

    Update CheckSpreadsheetOutput.R
    change variable names for readability
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- CheckSpreadsheetOutput.R
+++ CheckSpreadsheetOutput.R
@@ -7,11 +7,11 @@ old_nis <- read_xlsx("/conf/irf/03-Integration-Indicators/01-Core-Suite/Spreadsh
 
 
 checks <- left_join(new_nis, old_nis, by = c("year", "Time_Period", "Partnership", "Indicator"),
-                  suffix = c("new", "old"))
+                  suffix = c("_new", "_old"))
 
-checks <- checks %>% mutate(numdiff = label_percent(accuracy = 0.01)((numeratornew - numeratorold)/numeratorold),
-                        denomdiff = label_percent(accuracy = 0.01)((denominatornew - denominatorold)/denominatorold),
-                        valuediff = label_percent(accuracy = 0.01)((Ratenew - Rateold)/Rateold))
+checks <- checks %>% mutate(numdiff = label_percent(accuracy = 0.01)((numerator_new - numerator_old) / numerator_old),
+                        denomdiff = label_percent(accuracy = 0.01)((denominator_new - denominator_old) / denominator_old),
+                        valuediff = label_percent(accuracy = 0.01)((Rate_new - Rate_old)/Rate_old))
 
 check_list<- checks %>% split(checks$Indicator)
 
