80cbc76d7f89f06ced38c6e62b1097d5c86e4904
shintoLamp <bateman.mcbride@phs.scot>
2023-01-19 15:13:11 +0000

    New function to create Scotland variable for Tableau

---
+++ R/create_scotland_column.R
@@ -0,0 +1,16 @@
+#' Create an additional column with Scotland values for use in Tableau
+#'
+#' @param data A data frame containing rows where the partnership is Scotland
+#'
+#' @return A data frame with an additional variable, `scotland`, where the values
+#' can be matched on `year` and `time_period`
+#' @export
+create_scotland_column <- function(data) {
+
+  return_data <- data %>%
+    dplyr::filter(partnership == "Scotland") %>%
+    dplyr::rename(scotland = value) %>%
+    dplyr::select("year", "time_period", "scotland")
+
+  return(return_data)
+}
