1e4ed6635735856a45fbc4a6105f8bd2f865c967
James McMahon <james.mcmahon@phs.scot>
2023-04-06 10:09:23 +0100

    Expand `get_ni_dir` to optionally return the specific folder

--- R/get_file_paths.R
+++ R/get_file_paths.R
@@ -2,9 +2,26 @@
 #'
 #' @return The path to the NI folder
 #' @export
-get_ni_dir <- function() {
-  ni_dir <- fs::path("/", "conf", "irf", "03-Integration-Indicators", "01-Core-Suite")
-  return(ni_dir)
+get_ni_dir <- function(indicator = NULL) {
+  ni_dir <- fs::path(
+    "/",
+    "conf",
+    "irf",
+    "03-Integration-Indicators",
+    "01-Core-Suite"
+  )
+
+  if (!is.null(indicator)) {
+    ni_dir <- fs::path(
+      ni_dir,
+      dplyr::case_match(
+        indicator,
+        1:9 ~ "NI 1-9",
+        10:22 ~ paste("NI", indicator),
+        .default = NULL
+      )
+    )
+  }
 }
 
 #' NI Spreadsheet output dir
