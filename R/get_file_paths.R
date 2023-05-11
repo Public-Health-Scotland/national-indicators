f46b648b6e355adbdf59153ac14b3848676945ec
shintoLampgit config --global user.email bateman.mcbride@phs.scotm  git config --global user.name shintoLamp <bateman.mcbride@phs.scot>
2023-05-11 14:34:02 +0100

    Tidied up and added new output path to irf

--- R/get_file_paths.R
+++ R/get_file_paths.R
@@ -3,39 +3,33 @@
 #' @return The path to the NI folder
 #' @export
 get_ni_dir <- function(indicator = NULL) {
-  ni_dir <- fs::path(
-    "/",
-    "conf",
-    "irf",
-    "03-Integration-Indicators",
-    "01-Core-Suite"
-  )
-
-  if (!is.null(indicator)) {
-    ni_dir <- fs::path(
-      ni_dir,
-      dplyr::case_match(
-        indicator,
-        1:9 ~ "NI 1-9",
-        10:22 ~ paste("NI", indicator),
-        .default = NULL
-      )
-    )
-  }
+  ni_dir <- fs::path("/","conf","irf","03-Integration-Indicators","01-Core-Suite")
 }
+
+#' NI R output dir
+#'
+#' @return The path to the spreadsheet output folder
+#' @export
+get_ni_output_dir <- function() {
+  output_dir <- fs::path(get_ni_dir(), "outputs")
+  return(output_dir)
+}
+
 
 #' NI Spreadsheet output dir
 #'
 #' @return The path to the spreadsheet output folder
+#' @export
 get_ni_excel_output_dir <- function() {
-  output_dir <- fs::path(get_ni_dir(), "Spreadsheet outputs")
+  output_dir <- fs::path(get_ni_dir(), "spreadsheet_outputs")
   return(output_dir)
 }
 
 #' NI Tableau output dir
 #'
 #' @return The path to the Tableau output folder
+#' @export
 get_ni_tableau_output_dir <- function() {
-  output_dir <- fs::path(get_ni_dir(), "Tableau Outputs")
+  output_dir <- fs::path(get_ni_dir(), "tableau_outputs")
   return(output_dir)
 }
