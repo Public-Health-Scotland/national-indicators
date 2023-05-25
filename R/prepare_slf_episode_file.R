baf938c9393c296c4112746ef51259daa0e6f226
shintoLampgit config --global user.email bateman.mcbride@phs.scotm  git config --global user.name shintoLamp <bateman.mcbride@phs.scot>
2023-05-25 13:46:13 +0100

    Change to use most recent SLF

--- R/prepare_slf_episode_file.R
+++ R/prepare_slf_episode_file.R
@@ -8,24 +8,15 @@ prepare_slf_episode_file <- function(fin_year) {
   cost_names <- tolower(paste0(month.abb, "_cost"))
 
   slf <-
-    # slfhelper::read_slf_episode(fin_year, columns = c(
-    #   "year", "chi", "cij_marker", "cij_pattype", "cij_admtype", "age", "recid", "smrtype",
-    #   "record_keydate1", "record_keydate2",
-    #   "lca", "location", "datazone2011",
-    #   "yearstay", dplyr::all_of(cost_names)
-    # ), recids = c("01B", "04B", "GLS")) %>%
-    arrow::read_parquet("/conf/sourcedev/Source_Linkage_File_Updates/1718/source-episode-file-1718_ni_version.parquet",
-      col_select = c(
-        "year", "chi", "cij_marker", "cij_pattype", "cij_admtype", "age", "recid", "smrtype",
-        "record_keydate1", "record_keydate2",
-        "lca", "location", "datazone2011",
-        "yearstay", dplyr::all_of(cost_names)
-      )
-    ) %>%
+    slfhelper::read_slf_episode(fin_year, columns = c(
+      "year", "chi", "cij_marker", "cij_pattype", "cij_admtype", "age", "recid", "smrtype",
+      "record_keydate1", "record_keydate2",
+      "lca", "location", "datazone2011",
+      "yearstay", dplyr::all_of(cost_names)
+    ), recids = c("01B", "04B", "GLS")) %>%
     dplyr::mutate(cij_pattype = dplyr::if_else(.data$cij_admtype == 18, "Non-Elective", .data$cij_pattype)) %>%
     dplyr::filter(!is_missing(.data$chi) &
       !is_missing(.data$datazone2011) &
-      .data$recid %in% c("01B", "04B", "GLS") &
       .data$age >= 18 &
       !(.data$location %in% c("T113H", "S206H", "G106H")) &
       (.data$cij_pattype == "Non-Elective" & .data$smrtype %in% c("Acute-IP", "Psych-IP", "GLS-IP"))) %>%
