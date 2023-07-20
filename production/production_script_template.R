337c3358c3cccb58fc79c81b31311a9239600747
shintoLampgit config --global user.email bateman.mcbride@phs.scotm  git config --global user.name shintoLamp <bateman.mcbride@phs.scot>
2023-07-20 08:22:14 +0100

    WIP production script, added ni12 and ni13

--- production/production_script_template.R
+++ production/production_script_template.R
@@ -4,37 +4,29 @@
 library(lubridate) # Easier manipulation of dates
 
 # Fill in with years that are being updated for NI12 and NI13
-years_ni12_ni13 <- tibble::tribble(
-  ~year, ~year_start, ~year_end,
-  "1617", ymd("2016-04-01"), ymd("2017-03-31"),
-  "1718", ymd("2017-04-01"), ymd("2018-03-31"))
-#   "1819", ymd("2018-04-01"), ymd("2019-03-31"),
-#   "1920", ymd("2019-04-01"), ymd("2020-03-31"),
-#   "2021", ymd("2020-04-01"), ymd("2021-03-31"),
-#   "2122", ymd("2021-04-01"), ymd("2022-03-31"),
-#   "2223", ymd("2022-04-01"), ymd("2023-03-31"),
-#   "2324", ymd("2023-04-01"), ymd("2024-03-31"),
-# )
+years_ni12_ni13 <- c("1617", "1718")
 
 # Outcome indicators ----
 
-# NI12 and NI13 setup ----
-
-prepped_slfs <- lapply(tester_years$year, function(x) {
-  df <- prepare_slf_episode_file(x, ni_version = FALSE)
-})
-
-names(prepped_slfs) <- years_ni12_ni13$year
-
 # NI12 ----
 
-ni12_1617 <- calculate_ni12(prepped_slfs[["1617"]],
-                            fin_year_start = (dplyr::filter(years_ni12_ni13, year == "1617"))$year_start,
-                            fin_year_end = (dplyr::filter(years_ni12_ni13, year == "1617"))$year_end)
+ni12 <- lapply(years_ni12_ni13, function(x) {
+  df <- calculate_ni12(x, write_to_disk = FALSE)
+})
+
+names(ni12) <- years_ni12_ni13
 
 # NI13 ----
 
+ni13 <- lapply(years_ni12_ni13, function(x) {
+  df <- calculate_ni13(x, write_to_disk = FALSE)
+})
+
+names(ni13) <- years_ni12_ni13
+
 # NI14 ----
+
+ni14 <- calculate_ni14()
 
 # NI15 ----
 
